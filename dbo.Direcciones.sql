51c4171bdef98624bba60f7917aece5a2c6bf3c9
jimmy0890 <j_mcor@hotmail.com>
2016-02-01 21:10:22 -0600

    Base de datos

---
+++ dbo.Direcciones.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Dirreciones]
+(
+	[Primer Nombre] NVARCHAR(50) NOT NULL , 
+    [Apellido] NVARCHAR(50) NOT NULL, 
+    [Direccion] NVARCHAR(50) NULL, 
+    [Ciudad] NVARCHAR(50) NULL, 
+    [Telefono] NVARCHAR(50) NULL, 
+    [ID] NVARCHAR(50) NOT NULL, 
+    PRIMARY KEY ([Primer Nombre], [Apellido], [ID])
+)
---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Direcciones]
+(
+	[Primer Nombre] NVARCHAR(50) NULL , 
+    [Apellido] NVARCHAR(50) NULL, 
+    [Dirreccion] NVARCHAR(50) NULL, 
+    [Ciudad] NVARCHAR(50) NULL, 
+    [Telefono] NCHAR(10) NULL
+)
